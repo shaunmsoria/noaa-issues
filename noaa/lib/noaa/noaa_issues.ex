7d8b3d851fc970cc3cfac6739206d47139df7ba7
shaunmsoria <shaunmsoria@gmail.com>
2023-05-07 21:06:49 +0000

    extracted XML data to map

--- noaa/lib/noaa/noaa_issues.ex
+++ noaa/lib/noaa/noaa_issues.ex
@@ -6,26 +6,27 @@ defmodule Noaa.NoaaIssues do
     noaa_url(location)
     |> HTTPoison.get
     |> handle_response
-    |> convert_to_jason
+    |> convert_to_map
   end
 
   def noaa_url(location) do
     "https://w1.weather.gov/xml/current_obs/#{location}.rss"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_errors(),
+      body
+    }
   end
 
-  def handle_response({_, %{status_code: code, body: body}}) do
-    {:error, body }
-  end
+  defp check_for_errors(200),  do: :ok
+  defp check_for_errors(_),    do: :error
 
-  def convert_to_jason({:ok, xml_text}) do
-   xml_text
-   |> XmlToMap.naive_map()
-   |> Poison.encode!()
-   |> IO.inspect()
+  def convert_to_map({:ok, xml_text}) do
+   result =
+    xml_text
+    |> XmlToMap.naive_map()
 
   end
 
