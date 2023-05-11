0c0a7c54290fa5e96f73d43e2dd453ebed95b583
shaunmsoria <shaunmsoria@gmail.com>
2023-05-11 21:02:50 +0000

    Extracted information from data structure and reformating functions

--- noaa/lib/noaa/TableFormatter.ex
+++ noaa/lib/noaa/TableFormatter.ex
@@ -2,13 +2,29 @@ defmodule Noaa.TableFormatter do
   import Enum
 
   def print_table_for_columns(rows, header) do
-    rows    |> IO.inspect(label: "rows")
-    header  |> IO.inspect(label: "header")
-    access_table(rows, [Enum.at(header,0)]) |> IO.inspect(label: "#content_test")
+    with  {:ok, clear_header}   <- clean_header(header),
+          {:ok, table_content}          <- rows["current_observation"]["#content"],
+          {:ok, table_content}  <- content_extractor(header, rows)
+     do
+      access_table(rows, Enum.at(header, 0)) |> IO.inspect(label: "#content_test")
+
+      # table = rows["current_observation"]["#content"]
+      table
+      |> IO.inspect(label: "table content")
+      # Enum.at(header_element, 0)
+    end
   end
 
-  def access_table(rows, header_element) do
-    rows["current_observation"]["#content"][header_element]
+  def content_extractor(header, rows) do
+    {:ok, "test"}
+  end
+
+  def clean_header(header) do
+    {:ok, Enum.map(header, fn element -> Enum.at(String.split(element, "_string", trim: true), 0) end)}
+  end
+
+  def access_table(rows) do
+    {:ok, rows["current_observation"]["#content"]}
   end
 
 end
