57b206a6534a4d3ad602b6f13371584751cd68ea
shaunmsoria <shaunmsoria@gmail.com>
2023-05-16 20:54:52 +0000

    worked on formatting table pt1

--- noaa/lib/noaa/TableFormatter.ex
+++ noaa/lib/noaa/TableFormatter.ex
@@ -6,11 +6,9 @@ defmodule Noaa.TableFormatter do
           {:ok, table}            <- access_table(rows),
           {:ok, table_content}    <- content_extractor(header, clear_header, table),
           {:ok, table_all_string} <- table_string(table_content),
-          {:ok, table_length}     <- table_length_calculator(table_all_string)
+          {:ok, table_length}     <- table_length_calculator(table_all_string),
+          {:ok, table_formatted}  <- table_formater(table_all_string, table_length)
      do
-
-      table_content
-      |> IO.inspect(label: "table content")
 
       table_all_string
       |> IO.inspect(label: "the value of table_all_string is: ")
@@ -21,14 +19,52 @@ defmodule Noaa.TableFormatter do
     end
   end
 
-  def table_length_calculator(table_all_string) do
-    table_all_string
-    |> IO.inspect(label: "the value of table_all_string is: ")
+  def border_formater(length), do: String.pad_leading("", length, "-")
 
-    # why not a list error?
-
-    # {:ok, Enum.map(table_all_string, fn {key, value} -> Enum.max(length(key), length(value))end)}
+  def side_length(content, length_size) do
+    left_side_padding_size = Float.floor(length_size / 2, 0) - Float.floor(length(content) / 2, 0)
+    right_side_padding_size = Float.round(length_size / 2, 0) - Float.round(length(content) / 2, 0)
+    {:ok, [left_side_padding_size, right_side_padding_size]}
   end
+
+
+  def side_formater(content, length_size) do
+    with {:ok, side_length} = side_length(content, length_size) do
+      String.pad_leading(content, Enum.at(side_length, 0) + 1 , " ") <> content <> String.pad_trailing(content, Enum.at(side_length, 1) + 1, " ") <> "|"
+    end
+  end
+
+  def table_formater(table_all_string, table_length) do
+    with border_length = Enum.sum(table_length) + 2 * length(table_length),
+        border_string = border_formater(border_length) do
+          border_string
+          |> IO.inspect(label: "The value of border_string is: ")
+
+          # format each word in the table
+
+    end
+  end
+
+  def table_length_calculator(table_all_string) do
+    {:ok, Enum.map(table_all_string,
+      fn {key, value} ->
+        value0 =
+          String.split(value, "\n")
+          |> Enum.at(0)
+          |> String.length()
+
+        value1 =
+          String.split(value, "\n")
+          |> Enum.at(1)
+          |> value_nil
+          |> String.length()
+
+          Enum.max([String.length(key), value0 , value1])
+      end)}
+  end
+
+  def value_nil(nil),   do: ""
+  def value_nil(value), do: value
 
   def table_string(table_content) do
     {:ok, Enum.map(table_content, fn {key, value} -> {Atom.to_string(key), value} end)}
